ce9250d5fa7e5e06aa68e7c760e421c5aa274c68
Steve Bertrand <steveb@cpan.org>
2020-03-26 10:06:59 -0700

    Initial import

---
+++ RelayOTA.h
@@ -0,0 +1,15 @@
+#ifndef RELAYOTA_H
+#define RELAYOTA_H
+
+#include <Arduino.h>
+
+using namespace std;
+
+class RelayOTA {
+    public:
+        void begin ();
+        void otaSetup ();
+        void process ();
+};
+
+#endif
